2d7383e9433216667c257f32213c19f04bfc4560
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2024-02-02 00:15:24 +0200

    done

---
+++ 100-clean_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Keep it clean!
+import os
+from fabric.api import *
+
+env.hosts = ["54.160.85.72", "35.175.132.106"]
+
+
+def do_clean(number=0):
+    """do_clean"""
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
